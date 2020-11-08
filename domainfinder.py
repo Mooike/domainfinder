eff2223590853e3d6a4196bf88ed0ea5536c1204
mleins60 <mleins60@gmail.com>
2020-11-08 14:58:11 +0100

    main functionality

--- domainfinder.py
+++ domainfinder.py
@@ -1,5 +1,16 @@
+from selenium import webdriver
+
+
+
 
 #Get User Input
 print("Search for domain")
 user_input = input(": ")
 #print(user_input)
+
+#open godaddy.com
+browser = webdriver.Chrome(executable_path=r"C:\Users\mikel\Downloads\chromedriver_win32 (1)\chromedriver.exe")
+request =" https://de.godaddy.com/domainsearch/find?checkAvail=1&domainToCheck=" + user_input
+browser.get(request)
+
+print="----Look at Chrome----"
